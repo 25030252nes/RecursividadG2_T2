67a93f181496fc739a85056317021fb9acb7454a
25030252nes <25030252@itesa.edu.mx>
2026-09-13 16:31:38 -0600

    Version 03_:Sumar numeros.

--- Recursividad.Logica/Recursividades.cs
+++ Recursividad.Logica/Recursividades.cs
@@ -8,18 +8,44 @@ namespace Recursividad.Logica
     {
         public int CalcularFactorial(int num)
         {
-            //caso base o condicion de salida
-            if(num==1)
+            if (num == 1)
             {
                 return 1;
             }
 
-            //caso general
-
             return num * CalcularFactorial(num - 1);
-
         }
 
+        public int vocales(string cadena)
+        {
+            if (string.IsNullOrEmpty(cadena))
+            {
+                return 0;
+            }
 
+            char c = char.ToLower(cadena[0]);
+            int esVocal = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') ? 1 : 0;
+            return esVocal + vocales(cadena.Substring(1));
+        }
+
+        public int SumaDigitos(int num)
+        {
+            if (num < 10)
+            {
+                return num;
+            }
+
+            return (num % 10) + SumaDigitos(num / 10);
+        }
+
+        public int SumarNumeros(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+
+            return SumarNumeros(n - 1) + n;
+        }
     }
 }
--- Recursividad.UI.consola/Program.cs
+++ Recursividad.UI.consola/Program.cs
@@ -1,10 +1,36 @@
-﻿using Recursividad.Logica;
-
+﻿using System;
+using Recursividad.Logica;
 
 Console.WriteLine("Metodos recursivos");
 Console.WriteLine("\n\n calcular factorial de 5");
-
 Recursividades oRecursividades = new Recursividades();
 int resultado = oRecursividades.CalcularFactorial(5);
-
 Console.WriteLine("Factorial de 5 es:" + resultado);
+
+Console.WriteLine("\n--- 1. Contar Vocales ---");
+string texto = "Hola Mundo";
+int cantidadVocales = oRecursividades.vocales(texto);
+Console.WriteLine($"La cantidad de vocales en '{texto}' es: {cantidadVocales}");
+
+Console.WriteLine("\n--- 2. Suma de dígitos en secuencia ---");
+int[] secuencia = { 123, 99, 456, 801, 23 };
+int mayorNumero = 0;
+int mayorSuma = -1;
+
+foreach (int num in secuencia)
+{
+    int sumaActual = oRecursividades.SumaDigitos(num);
+    Console.WriteLine($"El número {num} tiene una suma de dígitos de: {sumaActual}");
+
+    if (sumaActual > mayorSuma)
+    {
+        mayorSuma = sumaActual;
+        mayorNumero = num;
+    }
+}
+Console.WriteLine($"El número con la mayor suma de dígitos es: {mayorNumero} (Suma: {mayorSuma})");
+
+Console.WriteLine("\n--- 3. Suma de primeros n números ---");
+int n = 5;
+int sumaTotal = oRecursividades.SumarNumeros(n);
+Console.WriteLine($"La suma de los primeros {n} números enteros es: {sumaTotal}");
